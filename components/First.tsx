d79aac54993a5d8e92cef9ee491ab3c792065843
Protyush-Dey <160174897+Protyush-Dey@users.noreply.github.com>
2026-03-01 20:47:34 +0530

    components

---
+++ components/First.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View ,Text} from 'react-native'
+
+function First () {
+  return (
+    <View>
+        <Text>1st</Text>
+    </View>
+  )
+}
+
+export default First
